359cc39b8dfc6a597ad2893f3ea5e8c705329155
BlkerGuy <2020.gafarov@gmail.com>
2023-04-13 06:04:17 +0400

    ddd

--- assets/Js/script.js
+++ assets/Js/script.js
@@ -1,10 +1,10 @@
 "use strict";
 
 
-let card_size_days=document.querySelectorAll('.card-size-other-days button');
+let card_size_days = document.querySelectorAll('.card-size-other-days button');
 
-for(let i=0; i<card_size_days.length; i++){
-    card_size_days[i].addEventListener('click',()=>{
+for (let i = 0; i < card_size_days.length; i++) {
+    card_size_days[i].addEventListener('click', () => {
         card_size_days[0].classList.remove("active")
     })
 }
@@ -17,17 +17,39 @@ let img = document.querySelector('.img');
 form.addEventListener('submit', async (e) => {
     try {
         e.preventDefault();
-        // img.src = 'https://i.stack.imgur.com/hzk6C.gif';
-        // const response = await fetch(`https://source.unsplash.com/random/900×700/?${inp.value}`);
-        // const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?id={${inp.value}}&appid={d479d3bf9c94ea562ecd0cc819118096}&lang={az}`);
-        const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${inp.value}&appid=d479d3bf9c94ea562ecd0cc819118096&lang=az&units=metric`);
-        console.log(response[0]);
+        const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${inp.value}&appid=d479d3bf9c94ea562ecd0cc819118096&lang=az&units=metric`)
+        // const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=Baku&appid=d479d3bf9c94ea562ecd0cc819118096&lang=az&units=metric`)
+        .then(function (resp) { return resp.json() })
+            .then(function (data) {
 
-        let country=document.querySelector('#country');
-        // country.innerText=response.arrayBuffer.prototype()
+                console.log(data);
+                
+                drawWeather(data);
+            })
 
-        
-        // img.src = response.url;
+        function drawWeather(d) {
+            // debugger;
+            var celcius = Math.round(parseFloat(d.main.temp));
+            var description = d.weather[0].description;
+
+            document.querySelector('#location').innerHTML =d.name + ", " + d.sys.country;
+            document.querySelector('#air-situation').innerHTML =description;
+            document.querySelector('#celcius').innerHTML =`${celcius}<sup>o</sup>C`;
+            document.querySelector('#humidity').innerHTML =`${d.main.humidity}%`;
+            document.querySelector('#wind').innerHTML =`${d.wind.speed}%`;
+            document.querySelector('#pressure').innerHTML =`${d.main.pressure}mbar`;
+
+
+            if (description.indexOf('rain') > 0) {
+                document.body.className = 'rainy';
+            } else if (description.indexOf('cloud') > 0) {
+                document.body.className = 'cloudy';
+            } else if (description.indexOf('sunny') > 0) {
+                document.body.className = 'sunny';
+            } else {
+                document.body.className = 'clear';
+            }
+        }
     } catch (err) {
         console.log(err);
     }
